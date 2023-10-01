a6a3f06618b8fc9f1ccc55bde9111c848ec03426
EXL <exlmotodev@gmail.com>
2023-10-01 11:12:29 +0700

    Fix PreLink.cmake script

--- ELFKIT_EP2_Windows/cmake/PreLink.cmake
+++ ELFKIT_EP2_Windows/cmake/PreLink.cmake
@@ -1,9 +1,9 @@
-function(redefine_sym SYM_IN SYM_OUT OBJECT_FILE)
+function(redefine_sym SYM_IN SYM_OUT PATCH_FILE EXT)
 	add_custom_command(
 		TARGET ${PROJECT_NAME}
 		PRE_LINK
-		COMMAND ${CMAKE_OBJCOPY} --redefine-sym ${SYM_IN}=${SYM_OUT} ${OBJECT_FILE}
-		COMMENT "Redefining ${SYM_IN} to ${SYM_OUT} in ${OBJECT_FILE}"
+		COMMAND ${CMAKE_OBJCOPY} --redefine-sym ${SYM_IN}=${SYM_OUT} "CMakeFiles/${PROJECT_NAME}.dir/${PATCH_FILE}${EXT}"
+		COMMENT "Redefining ${SYM_IN} to ${SYM_OUT} in ${PATCH_FILE}${EXT}"
 		VERBATIM
 	)
 endfunction()
